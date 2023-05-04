9edd634271b1a3b2982a38fb66d02934b3f743ba
Amina Cherni <cherniamina21@gmail.com>
2023-05-04 14:31:50 +0200

    ajout du user JPA

---
+++ src/main/java/fr/training/demo/api/UserController.java
@@ -0,0 +1,21 @@
+package fr.training.demo.api;
+
+import fr.training.demo.repository.UserRepository;
+import fr.training.demo.repository.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("test")
+    public ResponseEntity test() {
+       userRepository.save(new User("cherniii","sgs", LocalDate.now())) ;
+               return ResponseEntity.ok().build();
+    }
+}
